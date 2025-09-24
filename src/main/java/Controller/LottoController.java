8dd8277ecc5193f29ada497e14cafeb20072dd72
nonactress <tjgsuwls1007@gmail.com>
2025-09-25 01:14:54 +0900

    feat(controller) : making controller

---
+++ src/main/java/Controller/LottoController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import Model.Jackpot;
+import Model.LottoMatcher;
+import Model.Lottos;
+import View.InputView;
+import View.OutView;
+
+public class LottoController {
+    public void startLotto()
+    {
+        InputView inputView = new InputView();
+        int money = inputView.getMoney();
+
+        Lottos lottos = new Lottos(money);
+        lottos.printLottos();
+
+        Jackpot jackpot = new Jackpot(inputView.getJackpot());
+
+        LottoMatcher lottoMatcher = new LottoMatcher(lottos,jackpot);
+
+        OutView outView = new OutView();
+        outView.printLottoMatcher(lottoMatcher.getMatchCounts(), lottoMatcher.getRate(money));
+    }
+}
