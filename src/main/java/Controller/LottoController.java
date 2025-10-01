ee3d5459e9c22e9ae0ef26bf821716bf8e7a9e92
nonactress <tjgsuwls1007@gmail.com>
2025-10-02 03:00:28 +0900

    refactor(model) : - Apply updates from latest changes

--- src/main/java/Controller/LottoController.java
+++ src/main/java/Controller/LottoController.java
@@ -6,18 +6,29 @@ import Model.Lottos;
 import View.InputView;
 import View.OutView;
 
+import java.util.List;
+
 public class LottoController {
     public void startLotto() {
         InputView inputView = new InputView();
-        int money = inputView.getMoney();
 
-        Lottos lottos = new Lottos(money);
+        int money = inputView.getMoney();
+        int manualLottoNumber = inputView.getManualLottoNumber();
+
+        List<String> rawNumbersList = inputView.manualLotto(manualLottoNumber);
+
+        Lottos lottos = new Lottos(money, rawNumbersList);
 
         OutView outView = new OutView();
 
+        outView.printLottosStaus(lottos.getAutoLottoCount(money, manualLottoNumber), manualLottoNumber);
+
         outView.printLottos(lottos);
 
-        Jackpot jackpot = new Jackpot(inputView.getJackpot());
+        String[] winningNumber = inputView.getJackpotNumber();
+        int bonusNumber = inputView.getBonus();
+
+        Jackpot jackpot = new Jackpot(winningNumber, bonusNumber);
 
         LottoMatcher lottoMatcher = new LottoMatcher(lottos, jackpot);
 
