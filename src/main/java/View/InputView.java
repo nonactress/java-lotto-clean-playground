1bc5b7d9955ee3fbbc4216ff95f6ede78cc19aad
nonactress <tjgsuwls1007@gmail.com>
2025-10-02 02:51:31 +0900

    refactor(view) : class "OutView","InputView"

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,47 +1,60 @@
 package View;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 public class InputView {
     Scanner scanner = new Scanner(System.in);
 
     public int getMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        while(true)
-        {
-            try
-            {
-                int money =  scanner.nextInt();
+        while (true) {
+            try {
+                int money = scanner.nextInt();
                 validate(money);
                 return money;
-            }
-            catch (InputMismatchException e)
-            {
+            } catch (InputMismatchException e) {
                 System.out.println("금액은 숫자로 입력해주세요!");
                 scanner.nextLine();
-            }
-            catch (IllegalArgumentException e)
-            {
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
-
-
     }
 
-    private void validate(int money)
-    {
-        if(money<0)
+    private void validate(int money) {
+        if (money < 0)
             throw new IllegalArgumentException("금액이 0보다 작을 순 없습니다!");
-        if(money%1000!=0)
+        if (money % 1000 != 0)
             throw new IllegalArgumentException("1000원 단위로 입력해주세요!");
-
     }
 
-    public String[] getJackpot() {
-        scanner.nextLine();
+    public String[] getJackpotNumber() {
         System.out.println("정답 로또번호를 입력해주세요( ','로 구분해서 입력)");
         return scanner.nextLine().split(",");
+    }
+
+
+    public int getManualLottoNumber() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public List<String> manualLotto(int manualLottoNumber) {
+        List<String> manualLottoLines = new ArrayList<>();
+
+        scanner.nextLine();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        for (int i = 0; i < manualLottoNumber; i++) {
+            manualLottoLines.add(scanner.nextLine());
+        }
+        return manualLottoLines;
+    }
+
+    public int getBonus() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextInt();
     }
 }
--- src/main/java/View/OutView.java
+++ src/main/java/View/OutView.java
@@ -3,18 +3,25 @@ package View;
 import Model.Lotto;
 import Model.Lottos;
 
+import java.util.Map;
+
 public class OutView {
 
-    public void printLottoMatcher(int[] matchCounts, double rate) {
+    public void printLottoMatcher(Map<Integer, Integer> matchCounts, double rate) {
         System.out.println("당첨 통계");
         System.out.println("------");
-        System.out.println("3개 일치(5,000원): " + matchCounts[3] + "개");
-        System.out.println("4개 일치(50,000원): " + matchCounts[4] + "개");
-        System.out.println("5개 일치(1,500,000원): " + matchCounts[5] + "개");
-        System.out.println("6개 일치(2,000,000,000원): " + matchCounts[6] + "개");
+        System.out.println("3개 일치(5,000원): " + matchCounts.getOrDefault(3, 0) + "개");
+        System.out.println("4개 일치(50,000원): " + matchCounts.getOrDefault(4, 0) + "개");
+        System.out.println("5개 일치(1,500,000원): " + matchCounts.getOrDefault(5, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치(30,000,000원): " + matchCounts.getOrDefault(7, 0) + "개");
+        System.out.println("6개 일치(2,000,000,000원): " + matchCounts.getOrDefault(6, 0) + "개");
         System.out.printf("총 수익률은 %.2f입니다.", rate);
     }
 
+    public void printLottosStaus(int AutoLottoNumber, int manualLottoNumber) {
+        System.out.println();
+        System.out.println("수동으로 " + manualLottoNumber + "장, 자동으로" + AutoLottoNumber + "개를 구매했습니다.");
+    }
 
     public void printLottos(Lottos lottos) {
         for (Lotto lotto : lottos.getLottoList()) {
