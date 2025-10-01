4659d1a354d31ccec72cc40b5aa6016f437619e0
nonactress <tjgsuwls1007@gmail.com>
2025-10-02 02:56:29 +0900

    refactor(model) : Implement auto/manual lottery number generation logic

--- src/main/java/Model/Lottos.java
+++ src/main/java/Model/Lottos.java
@@ -8,16 +8,26 @@ public class Lottos {
     private List<Lotto> lottoList;
     private static final int COST_PER_TICKET = 1000;
 
-    public Lottos(int money) {
-        int numberOfLottos = money / COST_PER_TICKET;
+    public Lottos(int money, List<String> manualLottoLines) {
         this.lottoList = new ArrayList<>();
+        int manualLottosNumber = manualLottoLines.size();
 
-        for (int i = 0; i < numberOfLottos; i++) {
-            lottoList.add(new Lotto()); // new Lotto()가 호출될 때마다 새로운 로또 번호 6개가 생성됩니다.
+        // 수동 로또 생성
+        for (String line : manualLottoLines) {
+            lottoList.add(new ManualLotto(line));
+        }
+
+        // 자동 로또 생성
+        for (int i = 0; i < getAutoLottoCount(money,manualLottosNumber); i++) {
+            lottoList.add(new AutoLotto());
         }
     }
 
     public List<Lotto> getLottoList() {
         return Collections.unmodifiableList(lottoList);
+    }
+
+    public int getAutoLottoCount(int money,int manualLottosNumber) {
+        return money / COST_PER_TICKET - manualLottosNumber;
     }
 }
