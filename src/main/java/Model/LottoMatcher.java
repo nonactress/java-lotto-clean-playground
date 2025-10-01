46091b51cd26e1e5c43176ad3d6bf52ef3c9c052
nonactress <tjgsuwls1007@gmail.com>
2025-10-02 02:57:24 +0900

    refactor(model) : adding bonus ball calculation

--- src/main/java/Model/LottoMatcher.java
+++ src/main/java/Model/LottoMatcher.java
@@ -1,17 +1,28 @@
 package Model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoMatcher {
-    private final int[] matchCounts;
+    private final Map<Integer, Integer> matchCounts;
 
     public LottoMatcher(Lottos lottos, Jackpot jackpot) {
-        this.matchCounts = new int[7];
+        this.matchCounts = new HashMap<>();
 
         for (Lotto lotto : lottos.getLottoList()) {
             int matches = countMatches(jackpot.getJackpot(), lotto.getLotto());
-            matchCounts[matches]++;
+            checkBonus(jackpot, lotto, matches);
         }
+
+    }
+
+    private void checkBonus(Jackpot jackpot, Lotto lotto, int matches) {
+        if (matches == 5 && lotto.getLotto().contains(jackpot.getBounsNumber())) {
+            matchCounts.put(7, matchCounts.getOrDefault(7, 0) + 1);
+            return;
+        }
+        matchCounts.put(matches, matchCounts.getOrDefault(matches, 0) + 1);
     }
 
     private int countMatches(List<Integer> jackpotNumbers, List<Integer> lottoNumbers) {
@@ -22,6 +33,7 @@ public class LottoMatcher {
         return count;
     }
 
+
     private int getMatchValue(List<Integer> jackpotNumbers, int number) {
         if (jackpotNumbers.contains(number)) {
             return 1;
@@ -29,18 +41,21 @@ public class LottoMatcher {
         return 0;
     }
 
-    public int[] getMatchCounts() {
+    public Map<Integer, Integer> getMatchCounts() {
         return matchCounts;
     }
 
     public double calculateTotalEarnings() {
-        return    matchCounts[3] * PrizeMoney.THREE.getMoney()
-                + matchCounts[4] * PrizeMoney.FOUR.getMoney()
-                + matchCounts[5] * PrizeMoney.FIVE.getMoney()
-                + matchCounts[6] * PrizeMoney.SIX.getMoney();
+
+        return    matchCounts.getOrDefault(3, 0) * PrizeMoney.THREE.getMoney()
+                + matchCounts.getOrDefault(4, 0) * PrizeMoney.FOUR.getMoney()
+                + matchCounts.getOrDefault(5, 0) * PrizeMoney.FIVE.getMoney()
+                + matchCounts.getOrDefault(7, 0) * PrizeMoney.BONUS.getMoney()
+                + matchCounts.getOrDefault(6, 0) * PrizeMoney.SIX.getMoney();
     }
 
     public double getRate(int money) {
-        return (double) calculateTotalEarnings() / money;
+        return calculateTotalEarnings() / money;
     }
+
 }
