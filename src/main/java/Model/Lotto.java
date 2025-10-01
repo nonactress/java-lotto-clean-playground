28ce4b9c04a07313a8cfe232905e1cb57ae1c667
nonactress <tjgsuwls1007@gmail.com>
2025-10-02 02:59:41 +0900

    refactor(model) : use inherited parent object

--- src/main/java/Model/Lotto.java
+++ src/main/java/Model/Lotto.java
@@ -1,31 +1,17 @@
 package Model;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    private List<Integer> lotto;
-
-    public Lotto() {
-        List<Integer> numbers = new ArrayList<>();
-
-        for (int i = 1; i <= 45; i++) {
-            numbers.add(i);
-        }
-
-        Collections.shuffle(numbers);
-        this.lotto = new ArrayList<>(numbers.subList(0, 6));
-
-        Collections.sort(this.lotto); // 뽑은 6개의 숫자를 오름차순으로 정렬
-    }
+    protected List<Integer> numbers;
 
     public List<Integer> getLotto() {
-        return Collections.unmodifiableList(lotto);
+        return Collections.unmodifiableList(numbers);
     }
 
     @Override
     public String toString() {
-        return lotto.toString();
+        return numbers.toString();
     }
 }
