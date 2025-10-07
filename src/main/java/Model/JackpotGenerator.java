52e393bed2d396a59c4b5111aaac6269d8ccdbd0
nonactress <tjgsuwls1007@gmail.com>
2025-10-07 20:01:44 +0900

    refactor : using TreeSet

--- src/main/java/Model/JackpotGenerator.java
+++ src/main/java/Model/JackpotGenerator.java
@@ -2,9 +2,9 @@ package Model;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class JackpotGenerator {
     private final List<Integer> number;
@@ -14,37 +14,36 @@ public class JackpotGenerator {
     }
 
     public static JackpotGenerator from(String[] splits) {
-        if (splits.length != 6) {
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
-        }
-        List<Integer> numbers = parseNumbers(splits);
-        validateNumbers(numbers);
-        Collections.sort(numbers);
-        return new JackpotGenerator(Collections.unmodifiableList(numbers));
+        Set<Integer> numbers = parseAndValidateNumbers(splits);
+        return new JackpotGenerator(Collections.unmodifiableList(new ArrayList<>(numbers)));
     }
 
-    private static List<Integer> parseNumbers(String[] splits) {
-        List<Integer> numbers = new ArrayList<>();
+    private static Set<Integer> parseAndValidateNumbers(String[] splits) {
+        if (splits.length != 6) {
+            throw new IllegalArgumentException("당첨 번호는 6개를 입력해야 합니다.");
+        }
+
+        Set<Integer> numbers = new TreeSet<>();
         for (String s : splits) {
+            int number;
             try {
-                numbers.add(Integer.parseInt(s.trim()));
+                number = Integer.parseInt(s.trim());
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("당첨 번호는 숫자여야 합니다.");
             }
+            validateNumberRange(number);
+            numbers.add(number);
         }
-        return numbers;
-    }
-
-    private static void validateNumbers(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != 6) {
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
         }
 
-        for (int number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("당첨 번호는 1과 45 사이의 숫자여야 합니다.");
-            }
+        return numbers;
+    }
+
+    private static void validateNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("당첨 번호는 1과 45 사이의 숫자여야 합니다.");
         }
     }
 
